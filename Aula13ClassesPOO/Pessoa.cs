eb3e998ce13ff73055af9b289500e6f2ccf1bf6f
Juno <juno.estudos@outlook.com>
2023-08-31 16:37:10 -0300

    Add files via upload

---
+++ Aula13ClassesPOO/Pessoa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula13ClassesPOO
+{
+    internal class Pessoa
+    {
+        //Atributos
+        public string nome;
+        public int idade;
+        public string rua;
+        public string cidade;
+
+        //MÉTODO CONSTRUTOR
+        public Pessoa(string nome, int idade, string rua, string cidade) 
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.rua = rua;
+            this.cidade = cidade;
+        }
+
+        //Métodos
+        public void mostra()
+        {
+            Console.WriteLine($"Meu nome é {nome}, tenho {idade} anos e moro na {rua}, em {cidade}."); 
+        }
+    }
+}
---
+++ Aula13ClassesPOO/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Aula13ClassesPOO;
+
+Console.WriteLine("Hello, World!\n");
+
+Pessoa nicolas = new Pessoa("Nícolas", 18, "R. José Fernandes Nunes", "São Carlos");
+Pessoa andrey = new Pessoa("Andrey", 12, "R. José Fernandes Nunes", "São Carlos");
+Pessoa du = new Pessoa("Eduardo B.", 18, "Av. José Pereira Lopes", "São Carlos");
+Pessoa dudu = new Pessoa("Eduardo L.", 18, "Av. Comendador Alfredo Maffei", "São Carlos");
+
+List<Pessoa> pessoas= new List<Pessoa>();
+pessoas.Add(nicolas);
+pessoas.Add(andrey);
+pessoas.Add(du);
+pessoas.Add(dudu);
+
+for (int i = 0;i < pessoas.Count; i++)
+{
+    pessoas[i].mostra();
+    //Console.WriteLine(pessoas[i].nome);
+}
+
+Console.ReadKey();
